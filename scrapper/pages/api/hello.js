b508210909ca8584d88b88ee5d86e24c64b1a4e6
Arnav Gupta <info@arnavgupta.net>
2022-12-12 01:14:49 +0530

    database part 1 done

--- scrapper/pages/api/hello.js
+++ scrapper/pages/api/hello.js
@@ -1,6 +1,5 @@
-
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
+import { array } from "./email_data";
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  data = array;
+  res.status(200).json({ name: "John Doe" });
 }
