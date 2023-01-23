5cbe93167969c95b2b8d5656e81ca546f2068766
Arnav Gupta <info@arnavgupta.net>
2023-01-23 19:07:22 +0530

    sitemap error

--- snut_scrapper/pages/profile/[id].js
+++ snut_scrapper/pages/profile/[id].js
@@ -1,7 +1,20 @@
 import clientPromise from "../../middleware/mongodb";
 
 export default function Profile(props) {
-  return <div className="profile">{props.email}</div>;
+  return (
+    <div className="profile">
+      <Head
+        title={`Dope - ${props.email}@nsut.ac.in`}
+        description={`This is the profile of ${
+          props.name | "No name"
+        }. About - ${props.bio | "No Bio"}`}
+        kewrod={`, ${props.email}@nsut.ac.in, ${props.name}, profile`}
+        url={`profile/${props.email}`}
+        image="https://wwww.itsdope.in/social.jpg"
+      ></Head>
+      {props.email}
+    </div>
+  );
 }
 export async function getServerSideProps(id) {
   id = id.query.id;
