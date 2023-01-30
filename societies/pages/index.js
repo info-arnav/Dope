273f615b911a769315aa12b207db6ea74ab8f5d2
Arnav Gupta <info@arnavgupta.net>
2023-01-31 00:31:11 +0530

    about

--- societies/pages/index.js
+++ societies/pages/index.js
@@ -81,112 +81,127 @@ export default function Home({ username }) {
       });
     }
   }, [username]);
-  return username == null ? (
-    <></>
-  ) : username ? (
-    <div>
-      <nav>
-        <div className="soc-name">{username.toUpperCase()}</div>
-        <button
-          style={{ cursor: "pointer" }}
-          onClick={(e) => {
-            localStorage.removeItem("user");
-            reload();
-          }}
-        >
-          Logout
-        </button>
-      </nav>
-      {loading ? (
-        <div className="empty">
-          <Image src="/loading.gif" width={300} height={300}></Image>
-        </div>
-      ) : (
-        <div style={{ marginTop: 90 }}>
-          <form onSubmit={create}>
-            <center>
-              <p style={{ fontWeight: "bold", fontSize: 24 }}>New Post</p>
-              <input
-                className="inputs"
-                placeholder="title"
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-                required
-              ></input>
-              <textarea
-                placeholder="description"
-                value={description}
-                style={{ minHeight: 80 }}
-                className="inputs"
-                onChange={(e) => setDescription(e.target.value)}
-                required
-              ></textarea>
-              <br></br>
-              <p className="error-1">{error}</p>
-              <button action="submit" className="buttons" disabled={disabled}>
-                {disabled ? "Loading...." : "Add Event"}
-              </button>
-            </center>
-          </form>
-          <br></br>
-          <center>
-            <p style={{ fontWeight: "bold", fontSize: 24 }}>Past Posts</p>
-          </center>
-          <div className="grid">
-            {data.reverse().map((e) => (
-              <Link className="card" id="soc" href={`/post/${e._id}`}>
-                <div className="socs-name">{e.societies.toUpperCase()}</div>
-                <div className="soc-title">{e.title}</div>
-                <div className="soc-buttons">
+  return (
+    <>
+      <Head
+        title={`Dope Society`}
+        description={`This is the place where societies can provide updates to NSUTians.`}
+        keword={`, societies`}
+        url={``}
+        image={"https://www.itsdope.in/logo.png"}
+      ></Head>
+      {username == null ? (
+        <></>
+      ) : username ? (
+        <div>
+          <nav>
+            <div className="soc-name">{username.toUpperCase()}</div>
+            <button
+              style={{ cursor: "pointer" }}
+              onClick={(e) => {
+                localStorage.removeItem("user");
+                reload();
+              }}
+            >
+              Logout
+            </button>
+          </nav>
+          {loading ? (
+            <div className="empty">
+              <Image src="/loading.gif" width={300} height={300}></Image>
+            </div>
+          ) : (
+            <div style={{ marginTop: 90 }}>
+              <form onSubmit={create}>
+                <center>
+                  <p style={{ fontWeight: "bold", fontSize: 24 }}>New Post</p>
+                  <input
+                    className="inputs"
+                    placeholder="title"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    required
+                  ></input>
+                  <textarea
+                    placeholder="description"
+                    value={description}
+                    style={{ minHeight: 80 }}
+                    className="inputs"
+                    onChange={(e) => setDescription(e.target.value)}
+                    required
+                  ></textarea>
+                  <br></br>
+                  <p className="error-1">{error}</p>
                   <button
-                    onClick={(es) => {
-                      es.preventDefault();
-                      setId(e._id);
-                      update(e._id);
-                    }}
+                    action="submit"
+                    className="buttons"
                     disabled={disabled}
                   >
-                    {disabled ? "Loading...." : "Delete"}
+                    {disabled ? "Loading...." : "Add Event"}
                   </button>
-                  <button
-                    onClick={() => {
-                      router.push(`/post/${e._id}`);
-                    }}
-                  >
-                    View
-                  </button>
-                </div>
-              </Link>
-            ))}
-          </div>
+                </center>
+              </form>
+              <br></br>
+              <center>
+                <p style={{ fontWeight: "bold", fontSize: 24 }}>Past Posts</p>
+              </center>
+              <div className="grid">
+                {data.reverse().map((e) => (
+                  <Link className="card" id="soc" href={`/post/${e._id}`}>
+                    <div className="socs-name">{e.societies.toUpperCase()}</div>
+                    <div className="soc-title">{e.title}</div>
+                    <div className="soc-buttons">
+                      <button
+                        onClick={(es) => {
+                          es.preventDefault();
+                          setId(e._id);
+                          update(e._id);
+                        }}
+                        disabled={disabled}
+                      >
+                        {disabled ? "Loading...." : "Delete"}
+                      </button>
+                      <button
+                        onClick={() => {
+                          router.push(`/post/${e._id}`);
+                        }}
+                      >
+                        View
+                      </button>
+                    </div>
+                  </Link>
+                ))}
+              </div>
+            </div>
+          )}
         </div>
+      ) : (
+        <>
+          <center className="forms">
+            <form className="credentials" onSubmit={auth}>
+              <p className="title">Society Login</p>
+              <input
+                placeholder="User ID"
+                onChange={(e) => setUsernames(e.target.value)}
+                value={usernames}
+                type="text"
+                required
+              ></input>
+              <input
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                type="password"
+                required
+              ></input>
+              <p className="error-1">{error}</p>
+              <button action="submit" disabled={disabled}>
+                {disabled ? "Loading...." : "Login"}
+              </button>
+            </form>
+          </center>
+        </>
       )}
-    </div>
-  ) : (
-    <>
-      <center className="forms">
-        <form className="credentials" onSubmit={auth}>
-          <p className="title">Society Login</p>
-          <input
-            placeholder="User ID"
-            onChange={(e) => setUsernames(e.target.value)}
-            value={usernames}
-            type="text"
-            required
-          ></input>
-          <input
-            placeholder="Password"
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-            type="password"
-            required
-          ></input>
-          <p className="error-1">{error}</p>
-          <button action="submit" disabled={disabled}>
-            {disabled ? "Loading...." : "Login"}
-          </button>
-        </form>
-      </center>
     </>
   );
 }
