e4f9c4b8f42afdbd7eddbea0d98178540d35669f
Arnav Gupta <info@arnavgupta.net>
2023-01-31 17:27:56 +0530

    sitemap image

--- snut_scrapper/pages/api/sitemap-dynamic-images.xml.js
+++ snut_scrapper/pages/api/sitemap-dynamic-images.xml.js
@@ -7,7 +7,7 @@ export default async (req, res) => {
     const db = client.db("nsut");
     const data = await db.collection("users").find({}).toArray();
     let response = `<?xml version="1.0" encoding="UTF-8"?>
-                    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
+                    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">`;
     data.map((e) => {
       if (e.image) {
         response =
