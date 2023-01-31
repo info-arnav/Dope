7ab3c4a0fdfbab26095b76d5b1323e6d221603f4
Arnav Gupta <info@arnavgupta.net>
2023-01-31 13:51:46 +0530

    app api key

--- snut_scrapper/pages/api/profile.js
+++ snut_scrapper/pages/api/profile.js
@@ -5,8 +5,15 @@ export default async (req, res) => {
   try {
     const client = await clientPromise;
     const db = client.db("nsut");
-    const data = await db.collection("queries").find({}).toArray();
-    res.send(data.reverse());
+    const data = await db
+      .collection("users")
+      .find({ email: req.body.email })
+      .toArray();
+    if (data[0]) {
+      res.send(data[0]);
+    } else {
+      res.json({ error: true });
+    }
   } catch (e) {
     res.json({ error: true });
   }
