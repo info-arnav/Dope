b8ffc3e9d173afe7dee42a05ba055d5cf66db1fb
Arnav Gupta <60637717+info-arnav@users.noreply.github.com>
2023-02-03 15:45:54 +0530

    Update register.js

--- snut_scrapper/pages/register.js
+++ snut_scrapper/pages/register.js
@@ -152,7 +152,7 @@ export default function Login({ username_given }) {
             </form>
           ) : (
             <form className="credentials" onSubmit={auth}>
-              <p className="title">Register</p>
+              <p className="title">Reset Password</p>
               <input
                 placeholder="User ID"
                 onChange={(e) => setUsername(e.target.value)}
