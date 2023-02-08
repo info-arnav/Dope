6b8379c813be002a806b3d4cdb483fc42757c282
Arnav Gupta <info@arnavgupta.net>
2023-02-08 16:33:14 +0530

    check

--- snut_scrapper/pages/about.js
+++ snut_scrapper/pages/about.js
@@ -20,18 +20,48 @@ export default function About() {
         </div>
       </div>
       <p className="about-title">About</p>
-      <p className="about-content">
-        Dope assists NSUT students in making the transition to college, which is
-        a major adjustment in their lives. You can interact and socialise with
-        new students at the college by learning details about them, such as
-        their Instagram handles, checking out the latest news in society, and
-        even discussing any ideas you have in secret. <br></br>
-        <br></br>Before any freshmen register, the site already has some
-        predicated information about them; all they need to do is register using
-        their official email addresses to change that information. <br></br>
-        <br></br>
-        Need more? Simply post your ideas in the site's anonymous section.
-      </p>
+      <div class="masonry-container">
+        <div class="masonry-item">
+          <a href="https://uk.pinterest.com/pin/436286282624249460/">
+            <img src="https://s-media-cache-ak0.pinimg.com/736x/61/4f/1b/614f1b3a481f68b4d492f71c5fe3fc5f.jpg" />
+          </a>
+          <span>Img 1</span>
+        </div>
+
+        <div class="masonry-item">
+          <a href="https://uk.pinterest.com/pin/481040803922110845/">
+            <img src="https://s-media-cache-ak0.pinimg.com/736x/1d/6f/2d/1d6f2d4fff0f0d4e83456c28b0645bbd.jpg" />
+          </a>
+          <span>Img 2</span>
+        </div>
+
+        <div class="masonry-item">
+          <a href="http://grrlathr.com/woodworkingstand/picture-frame-design-on-walls-plans-diy-free-download-plans-to-build-bunk-beds">
+            <img src="http://i00.i.aliimg.com/wsphoto/v0/1913341970_3/New-Design-Photo-Frame-Wall-Decals-Memories-Photo-Frame-Vinyl-Wall-Stickers-Home-Decor-Witness-Every.jpg" />
+          </a>
+          <span>Img 3</span>
+        </div>
+        <div class="masonry-item">
+          <a href="https://uk.pinterest.com/pin/436286282624249460/">
+            <img src="https://s-media-cache-ak0.pinimg.com/736x/61/4f/1b/614f1b3a481f68b4d492f71c5fe3fc5f.jpg" />
+          </a>
+          <span>Img 4</span>
+        </div>
+
+        <div class="masonry-item">
+          <a href="https://uk.pinterest.com/pin/481040803922110845/">
+            <img src="https://s-media-cache-ak0.pinimg.com/736x/1d/6f/2d/1d6f2d4fff0f0d4e83456c28b0645bbd.jpg" />
+          </a>
+          <span>Img 5</span>
+        </div>
+
+        <div class="masonry-item">
+          <a href="http://grrlathr.com/woodworkingstand/picture-frame-design-on-walls-plans-diy-free-download-plans-to-build-bunk-beds">
+            <img src="http://i00.i.aliimg.com/wsphoto/v0/1913341970_3/New-Design-Photo-Frame-Wall-Decals-Memories-Photo-Frame-Vinyl-Wall-Stickers-Home-Decor-Witness-Every.jpg" />
+          </a>
+          <span>Img 6</span>
+        </div>
+      </div>
     </div>
   );
 }
