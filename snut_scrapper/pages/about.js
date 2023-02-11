3226144fc3692a333f104e8e9f2995db1d378516
Arnav Gupta <info@arnavgupta.net>
2023-02-12 02:11:13 +0530

    changes

--- snut_scrapper/pages/about.js
+++ snut_scrapper/pages/about.js
@@ -21,7 +21,7 @@ export default function About({ username_given }) {
           </div>
           <p className="about-content">
             Dope is a platform that makes your transition to university just
-            that more seamless.
+            that much more seamless.
           </p>
           <p className="about-content large">
             Be it keeping upto date with the hectic schedules of societies,
