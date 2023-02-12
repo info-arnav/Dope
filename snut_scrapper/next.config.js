e740fa5bfd33160a4d8d56b97f537f30a657441b
Arnav Gupta <info@arnavgupta.net>
2023-02-13 00:33:55 +0530

    headers

--- snut_scrapper/next.config.js
+++ snut_scrapper/next.config.js
@@ -1,7 +1,52 @@
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self';
+  child-src www.itsdope.in;
+  style-src 'self' www.itsdope.in;
+  font-src 'self';  
+`;
+
 module.exports = {
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
+  },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
+          },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
+          },
+          {
+            key: "X-DNS-Prefetch-Control",
+            value: "on",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "origin-when-cross-origin",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
+          },
+        ],
+      },
+    ];
   },
   images: {
     remotePatterns: [
